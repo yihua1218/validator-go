f6b4c9dbe4a2dcf081e29442aee09d8e7a288c47
Yi-Hua Liang <yihua1218@gmail.com>
2018-06-22 08:06:07 +0800

    Add root foreach.

--- validator.go
+++ validator.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 
 	govalid "github.com/gima/govalid/v1"
+	"github.com/tidwall/gjson"
 )
 
 // Validator is a struct for validation
@@ -13,6 +14,7 @@ type Validator struct {
 	File   string
 	JSON   map[string]interface{}
 	Schema govalid.Validator
+	root   gjson.Result
 }
 
 var (
@@ -23,13 +25,21 @@ var (
 )
 
 // New is for creating a new validator
-func New(filename string) Validator {
+func New(filename string) (Validator, error) {
 	validator.File = filename
 	file, e := ioutil.ReadFile(validator.File)
 
 	if e != nil {
 		fmt.Printf("File error: %v\n", e)
+	} else {
+		validator.root = gjson.Parse(string(file))
+
+		validator.root.ForEach(func(key, value gjson.Result) bool {
+			fmt.Printf("key: %s\n", key)
+			return true
+		})
+
+		json.Unmarshal(file, &validator.JSON)
 	}
-	json.Unmarshal(file, &validator.JSON)
-	return validator
+	return validator, e
 }
